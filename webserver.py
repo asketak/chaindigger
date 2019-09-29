74bc746b3e63305d45cdbe97709c28e953b88f8a
asketak <simatomas@seznam.cz>
2019-09-29 13:47:33 +0200

    sg

--- webserver.py
+++ webserver.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 from neo4j import GraphDatabase
 from pprint import pprint
 from flask import request
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
